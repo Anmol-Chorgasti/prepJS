615ed8dfa548621d073559ca136540251cea6016
Anmol Dilipkumar Chorgasti <anmolchorgasti@gmail.com>
2023-06-08 10:43:57 +0200

    exercises in the basics and variables chapter

---
+++ preparations_exercise/exercises.js
@@ -0,0 +1,9 @@
+let fname = "John";
+let lname = "Doe";
+
+let fullname = fname + " " + lname;
+console.log(fullname);
+
+let number = 4936;
+
+console.log("thousands place is " + parseInt(number/1000));
---
+++ preparations_exercise/greeter.js
@@ -0,0 +1,5 @@
+let greetingname = "Victor";
+
+console.log("Good Morning, " + greetingname);
+console.log("Good Afternoon, " + greetingname);
+console.log("Good Evening, " + greetingname);
